c70aa820616e0f4e104cd883de72d3dff209b29f
= <=>
2021-09-16 16:04:43 +0900

    Enter

--- SketchFilter.py
+++ SketchFilter.py
@@ -4,7 +4,7 @@ from tkinter.filedialog import askopenfilename
 import cv2
 
 DELTA = 0
-KSIZE = 5
+KSIZE = 7
 
 tkinter.Tk().withdraw()
 filename = askopenfilename(title = 'Select input a Image')
@@ -15,13 +15,11 @@ height = im.shape[0]
 hueIm = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 h1, s1, v = cv2.split(hueIm)
 
-r = im[:, :, 2].copy()
-g = im[:, :, 1].copy()
-b = im[:, :, 0].copy()
 
 me = np.finfo(float).eps
 
 v1 = v.copy()
+bumwi = KSIZE+2*(KSIZE//2)
 
 def sketchFilter(value):
     for a in range(KSIZE // 2, height - KSIZE // 2):
@@ -31,23 +29,35 @@ def sketchFilter(value):
                 for f in range(-KSIZE // 2 + 1, KSIZE // 2 + 1):
                     d.append(v[a + c, b + f])
             value[a, b] = value[a, b] / (max(d) + me + DELTA)*255
+    
+    return value
 
 
-sketchFilter(v1)
-sketchFilter(r)
-sketchFilter(g)
-sketchFilter(b)
+
+
+def sketchFilter2(value):
+    for a in range(bumwi // 2, height - bumwi // 2):
+        for b in range(bumwi // 2, width - bumwi // 2):
+            d = []
+            for c in range(-bumwi // 2 + 1, bumwi // 2 + 1, 2):
+                for f in range(-bumwi // 2 + 1, bumwi // 2 + 1, 2):
+                    d.append(v[a + c, b + f])
+            value[a, b] = value[a, b] / (max(d) + me + DELTA)*255
+    return value
+
+v1 = sketchFilter(v1)
+v2 = sketchFilter2(v.copy())
+
 
 v1 = np.clip(v1, 0, 255)
-r = np.clip(r, 0, 255)
-g = np.clip(g, 0, 255)
-b = np.clip(b, 0, 255)
+v2 = np.clip(v2, 0, 255)
+
 print("그림을 준비 중입니다")
 
 result1 = cv2.merge([h1, s1, v1])
-result2 = cv2.merge([b, g, r])
+result2 = cv2.merge([h1, s1, v2])
 result1 = cv2.cvtColor(result1, cv2.COLOR_HSV2BGR)
-
+result2 = cv2.cvtColor(result2, cv2.COLOR_HSV2BGR)
 (h, w) = result1.shape[:2]
 max_size = 600
 
@@ -76,9 +86,9 @@ result1 = cv2.resize(result1, (nw, nh))
 result2 = cv2. resize(result2, (nw, nh))
 
 cv2.imshow("Original", im)
-cv2.imshow("HSV My Hist", result1)
+cv2.imshow("filter", result1)
 cv2.imwrite('result1.png', result1)
-cv2.imshow("RGB My Hist", result2)
+cv2.imshow("expandableFilter", result2)
 cv2.imwrite('result2.png', result2)
 cv2.waitKey()
 cv2.destroyAllWindows()
